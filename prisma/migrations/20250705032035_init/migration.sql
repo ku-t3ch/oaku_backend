19ed2de8a4c98f8556743aa093f502eff2cf283b
G.gorfor <rawiponponsarutwanit@gmail.com>
2025-07-06 00:35:38 +0700

    fix docker-compose and migration

---
+++ prisma/migrations/20250705032035_init/migration.sql
@@ -0,0 +1,185 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN', 'CAMPUS_ADMIN', 'SUPER_ADMIN');
+
+-- CreateEnum
+CREATE TYPE "Position" AS ENUM ('HEAD', 'MEMBER', 'NON_POSITION');
+
+-- CreateEnum
+CREATE TYPE "ComplianceStandard" AS ENUM ('KNOWLEDGE', 'SKILLS', 'ETHICS', 'PERSONAL_CHARACTERISTICS');
+
+-- CreateEnum
+CREATE TYPE "KasetsartStudentIdentity" AS ENUM ('INTEGRITY', 'DETERMINATION', 'KNOWLEDGE_CREATION', 'UNITY');
+
+-- CreateEnum
+CREATE TYPE "SDG" AS ENUM ('SDG1', 'SDG2', 'SDG3', 'SDG4', 'SDG5', 'SDG6', 'SDG7', 'SDG8', 'SDG9', 'SDG10', 'SDG11', 'SDG12', 'SDG13', 'SDG14', 'SDG15', 'SDG16', 'SDG17');
+
+-- CreateTable
+CREATE TABLE "Campus" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Campus_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OrganizationType" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "campusId" TEXT NOT NULL,
+
+    CONSTRAINT "OrganizationType_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Organization" (
+    "id" TEXT NOT NULL,
+    "publicOrganizationId" TEXT NOT NULL,
+    "nameEn" TEXT NOT NULL,
+    "nameTh" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "details" TEXT NOT NULL,
+    "socialMedia" JSONB,
+    "email" TEXT NOT NULL,
+    "phoneNumber" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "campusId" TEXT NOT NULL,
+    "organizationTypeId" TEXT NOT NULL,
+
+    CONSTRAINT "Organization_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phoneNumber" TEXT,
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "isSuspended" BOOLEAN NOT NULL DEFAULT false,
+    "campusId" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "publicProjectId" TEXT NOT NULL,
+    "activityCode" TEXT NOT NULL,
+    "nameEn" TEXT NOT NULL,
+    "nameTh" TEXT NOT NULL,
+    "dateStart" TIMESTAMP(3) NOT NULL,
+    "dateEnd" TIMESTAMP(3) NOT NULL,
+    "targetUser" JSONB NOT NULL,
+    "participants" JSONB,
+    "schedule" JSONB,
+    "principlesAndReasoning" TEXT,
+    "budgetUsed" INTEGER,
+    "objectives" TEXT,
+    "activityFormat" TEXT[],
+    "expectedProjectOutcome" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "location" JSONB,
+    "organizationId" TEXT NOT NULL,
+    "campusId" TEXT NOT NULL,
+    "complianceStandards" "ComplianceStandard"[],
+    "kasetsartStudentIdentities" "KasetsartStudentIdentity"[],
+    "sustainableDevelopmentGoals" "SDG"[],
+    "activityHours" JSONB,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ActivityHour" (
+    "id" TEXT NOT NULL,
+    "isCompleted" BOOLEAN NOT NULL DEFAULT false,
+    "fileNamePrinciple" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "projectId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "ActivityHour_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Log" (
+    "id" TEXT NOT NULL,
+    "action" TEXT NOT NULL,
+    "message" TEXT,
+    "userId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Log_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserOrganization" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "organizationId" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+    "position" "Position" NOT NULL,
+    "joinedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "UserOrganization_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Organization_publicOrganizationId_key" ON "Organization"("publicOrganizationId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Organization_email_key" ON "Organization"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_userId_key" ON "User"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_publicProjectId_key" ON "Project"("publicProjectId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_activityCode_key" ON "Project"("activityCode");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserOrganization_userId_organizationId_key" ON "UserOrganization"("userId", "organizationId");
+
+-- AddForeignKey
+ALTER TABLE "OrganizationType" ADD CONSTRAINT "OrganizationType_campusId_fkey" FOREIGN KEY ("campusId") REFERENCES "Campus"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Organization" ADD CONSTRAINT "Organization_campusId_fkey" FOREIGN KEY ("campusId") REFERENCES "Campus"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Organization" ADD CONSTRAINT "Organization_organizationTypeId_fkey" FOREIGN KEY ("organizationTypeId") REFERENCES "OrganizationType"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_campusId_fkey" FOREIGN KEY ("campusId") REFERENCES "Campus"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_campusId_fkey" FOREIGN KEY ("campusId") REFERENCES "Campus"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ActivityHour" ADD CONSTRAINT "ActivityHour_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ActivityHour" ADD CONSTRAINT "ActivityHour_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Log" ADD CONSTRAINT "Log_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserOrganization" ADD CONSTRAINT "UserOrganization_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserOrganization" ADD CONSTRAINT "UserOrganization_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE CASCADE ON UPDATE CASCADE;
